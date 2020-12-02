7863fcdcc3fdd67df588a66f55fcb75f78cedf4a
Christiano0407 <placebo0704@gmail.com>
2020-12-02 09:43:10 -0600

    video

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
-video.addEventListener(`click`, () => {
+video.addEventListener(`click`, function() {
     console.log("Click");
-})
+    this.classList.add("hidden");
+    import(`./player.js`).then(({player}) => {
+       // console.log(player);
+       setTimeout(() => {
+        player.play();
+       }, 100)
+    })
+});
