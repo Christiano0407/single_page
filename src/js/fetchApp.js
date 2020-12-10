881d937d463a290ea858ad5116570061e5cd42bc
Christiano0407 <placebo0704@gmail.com>
2020-12-10 08:51:15 -0600

    promises-fetch

--- src/js/fetchApp.js
+++ src/js/fetchApp.js
@@ -1,11 +1,16 @@
-const pokemon = document.querySelector(`#pokemon`);
+const $pokemon = document.querySelector(`#pokemon`);
 //const click = document.getElementById(`pokemon`)
 const click = document.addEventListener(`click`, () => {
-    console.log("Le diste un click");
+    console.log("Le diste un click")
 })
+const $message = document.querySelector(`#message`);
 
 function renderPokemon(image){
-    pokemon.setAttribute(`src`, image)
+    $pokemon.setAttribute(`src`, image)
+}
+
+function renderMessage(message){
+    $message.textContent = message
 }
 //
 fetch(`https://pokeapi.co/api/v2/pokemon/25/`)
@@ -13,6 +18,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon/25/`)
      .then(pokemon => {
          renderPokemon(pokemon.sprites.front_default)
      })
+     .catch(() => {renderMessage(`Pokemon no encontrado :( `)})
 //
 /* fetch(`pokemon.json`,{
 
