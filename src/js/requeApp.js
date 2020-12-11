b13a1b7aa01585f375734ab261226c5d0c442d44
Christiano0407 <placebo0704@gmail.com>
2020-12-11 08:31:49 -0600

    finished-ajax

--- src/js/requeApp.js
+++ src/js/requeApp.js
@@ -1,28 +1,50 @@
-const BASE_URL = `http//pokeapi.co/api/v2`;
-const $image = document.querySelector(`#image`);
-const $name = document.querySelector(`#name`);
+const BASE_URL = 'https://pokeapi.co/api/v2/'
+    const $image = document.querySelector('#image')
+    const $name = document.querySelector('#name')
 
-function renderError(status) {
-    $name.textContent = `${status} No encontado`
-}
+    function renderError(status) {
+      $name.textContent = `${status} Pokemon no encontrado`
+    }
+    function renderPokemon(pokemon) {
+      $name.textContent = pokemon.name
+      $image.setAttribute('src', pokemon.sprites.front_default)
+    }
 
-function renderPok(pokemon) {
-    $image.setAttribute(`src`, pokemon.sprites.front_default)
-}
+function ajax({ url, method = 'GET', async = true, done = () => {}, error = () => {}, responseType = 'json' }) {
+    function status(readyState) {
+      switch(readyState) {
+        case 0: return 'uninitilized'
+        case 1: return 'loading'
+        case 2: return 'loaded'
+        case 3: return 'interactive'
+        case 4: return 'completed'
+      }
+    }
+    const request = new XMLHttpRequest()
+    request.responseType = responseType
+    console.log(status(request.readyState), request.readyState)
 
-function ajax({url, method = `GET`, async = true, done = (), error = () => {}, responseType = `jason`}) {
- function status(readyState) {
-     switch(readyState) {
-         case 0: return `uninitilized`
-         case 1: return `loading`
-         case 2: return  `loaded`
-         case 3: return `interactive`
-         case 4: return `completed`
-     }
- }
-}
+    request.onreadystatechange = () => {
+      console.log(status(request.readyState), request.readyState)
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          done(request.response)
+        } else {
+          error(request.status)
+        }
+      }
+    }
+    request.open(method, url, async)
+    request.send(null)
+  }
 
-const request = new XMLHttpRequest()
-request.responseType = responseType
-console.log(status(request.readyState), request.readyState)
+  ajax({
+    url: `${BASE_URL}pokemon/745`,
+    // method: 'GET',
+    // async: true,
+    // responseType: 'json',
+    done: renderPokemon,
+    error: renderError,
+  })
+
 
