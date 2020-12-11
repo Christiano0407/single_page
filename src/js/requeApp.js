970d765bd873f09a0f99edc5c3d37a13bc1b51a6
Christiano0407 <placebo0704@gmail.com>
2020-12-10 21:53:29 -0600

    request-1

--- src/js/requeApp.js
+++ src/js/requeApp.js
@@ -3,5 +3,23 @@ const $image = document.querySelector(`#image`);
 const $name = document.querySelector(`#name`);
 
 function renderError(status) {
-    
+    $name.textContent = `${status} No encontado`
 }
+
+function renderPok(pokemon) {
+    $image.setAttribute(`src`, pokemon.sprites.front_default)
+}
+
+function ajax({url, method = `GET`, async = true, done = (), error = () => {}, responseType = `jason`}) {
+ function status(readyState) {
+     switch(readyState) {
+         case 0: return `uninitilized`
+         case 1: return `loading`
+         case 2: return  `loaded`
+         case 3: return `interactive`
+         case 4: return `completed`
+     }
+ }
+}
+
+const request = new XMLHttpRequest()
