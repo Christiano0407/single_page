894a8c5287c089e89ee1d8219ebee976999a12f2
Christiano0407 <placebo0704@gmail.com>
2020-12-10 21:41:44 -0600

    xml

---
+++ src/js/requeApp.js
@@ -0,0 +1,7 @@
+const BASE_URL = `http//pokeapi.co/api/v2`;
+const $image = document.querySelector(`#image`);
+const $name = document.querySelector(`#name`);
+
+function renderError(status) {
+    
+}
