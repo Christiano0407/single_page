7a6fa33bf584bf9ca0f3316b173cd2683604dfa3
Christiano0407 <placebo0704@gmail.com>
2020-12-14 12:25:32 -0600

    video-click

--- src/js/mainJS.js
+++ src/js/mainJS.js
@@ -1,2 +1,17 @@
 //Javascript para REACT
 // DYNAMIC IMPORTS
+/* video.addEventListener(`click`, function() {
+    console.log("Click");
+    this.classList.add("hidden");
+    import(`./player.js`).then(({player}) => {
+       // console.log(player);
+       setTimeout(() => {
+        player.play();
+       }, 100)
+    })
+}); */
+
+// 
+video.addEventListener(`click`, function(){
+    console.log(`Click`);
+})
